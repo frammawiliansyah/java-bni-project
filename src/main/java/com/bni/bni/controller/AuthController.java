49c8f6042ef6922bb0e3f2b085572cc9ea46a51d
Framma <iamframma@gmail.com>
2025-06-04 08:13:11 +0700

    update new build

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
 import java.util.HashMap;
